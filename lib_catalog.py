aad3738ccea178e7baa4a5200f109b6755280e0a
iamnovichek <rungotika@gmail.com>
2023-04-11 22:32:32 +0200

    library methods

--- lib_catalog.py
+++ lib_catalog.py
@@ -1,4 +1,6 @@
 from peewee import *
+from abc import ABC
+import json
 
 # date manager
 
@@ -6,12 +8,30 @@ from peewee import *
 DB = SqliteDatabase('library.db')
 
 
+# DB.connect()
+# DB.create_tables(['members', 'books', 'checked_out_books'])
+
+class DataManager(ABC):
+    def add_book(self, *args, **kwargs):
+        pass
+
+    def add_member(self, *args, **kwargs):
+        pass
+
+    def delete_member(self, *args, **kwargs):
+        pass
+
+
 class Member(Model):
-    user_id = AutoField(primary_key=True,
-                        unique=True)
+    member_id = AutoField(primary_key=True,
+                          unique=True)
     name = CharField(max_length=60)
     address = CharField(max_length=255)
-    email = CharField(max_length=255)
+    email = CharField(max_length=255,
+                      unique=True)
+
+    def __str__(self):
+        return f'{self.name}'
 
     class Meta:
         database = DB
@@ -25,7 +45,11 @@ class Book(Model):
     author = CharField(max_length=30)
     publisher = CharField(max_length=100)
     ISBN = CharField(max_length=13)
-    checked_out = BooleanField()
+    # checked_out = BooleanField() --> to do
+    borrowers = TextField()  # will be a json array
+
+    def __str__(self):
+        return f'Title: {self.title}/ Author: {self.author}'
 
     class Meta:
         database = DB
@@ -33,7 +57,7 @@ class Book(Model):
 
 
 class CheckedOutBook(Model):
-    user_id = ForeignKeyField(Member)
+    user_id = ForeignKeyField(Member, backref='member_id')
     book_id = ForeignKeyField(Book)
 
     class Meta:
@@ -41,135 +65,99 @@ class CheckedOutBook(Model):
         table_name = 'checked_out_books'
 
 
-class DataManager:
-
-    def add_book(self, book: object,
-                 quantity: int = 1):
-        pass
-
-    def add_member(self, member: object):
-        pass
-
-    def find_member_by_id(self):
-        pass
-
-    def delete_member(self, member: object):
-        pass
-
-
-class Book:
-    # I think It's done
-    def __init__(self,
-                 TITLE: str,
-                 AUTHOR: str,
-                 PUBLISHER: str,
-                 ISBN: int,
-                 borrowers: list = None
-                 ):
-        self.title = TITLE
-        self.author = AUTHOR
-        self.publisher = PUBLISHER
-
-        # must have 13 ints
-        self.ISBN = ISBN
-        self.borrowers = borrowers
-
-    def __str__(self):
-        return f'Name: {self.title}/ Author: {self.author}'
-
-    def __repr__(self):
-        return f'Book(TITLE="{self.title}", ' \
-               f'AUTHOR="{self.author}", ' \
-               f'PUBLISHER="{self.publisher}", ' \
-               f'borrowers="{self.borrowers}")'
-
-
 class Library(DataManager):
     # must have checkout system
-    books = []
-    members = []
 
     @classmethod
-    def add_book_to_db(cls,
-                       title: str,
-                       author: str,
-                       publisher: str,
-                       ISBN: int):
+    def add_book(cls,
+                 title: str,
+                 author: str,
+                 publisher: str,
+                 ISBN: int):
         assert len(str(ISBN)) == 13, 'Enter 13 numbers'
-        new_book = Book(
-            TITLE=title,
-            AUTHOR=author,
-            PUBLISHER=publisher,
-            ISBN=ISBN
+        book = Book.create(
+            title=title,
+            author=author,
+            publisher=publisher,
+            ISBN=ISBN,
+            borrowers=None
+            # my_list = ['item 1', 'item 2', 'item 3']
+            # json_data = json.dumps(my_list)
         )
+        book.save()
 
-        cls._db.add_book(new_book)
-
-        return new_book
+        return book
 
     @classmethod
-    def create_new_member(cls,
-                          name,
-                          email,
-                          address):
-        new_member = Member(
+    def add_member(cls,
+                   name: str,
+                   email: str,
+                   address: str):
+        member = Member.create(
             name=name,
             email=email,
             address=address
         )
+        member.save()
 
-        cls._db.add_member(new_member)
+        return member
 
-        return new_member
+    @classmethod
+    def delete_member(cls, email: str):
+        member = Member.select().where(Member.email == email).get()
+        member.delete_instance()
 
-    def delete_member(self):
-        pass
+        return member
+
+    @classmethod
+    def list_members(cls):
+        all_members = Member.select()
+
+        return all_members
+
+    @classmethod
+    def list_books(cls):
+        all_books = Book.select('title', 'author')
+
+        return all_books
+
+    @classmethod
+    def search_book_by_title(cls, title: str):
+        book = Book.select('title').where(Book.title == title).get()
+
+        return book
+
+    @classmethod
+    def search_books_by_author(cls, author: str):
+        books = Book.select('author').where(Book.author == author).get()
+
+        return books
+
+    @classmethod
+    def get_popular_books(cls):
+        """This method will give 10 latest books checked out"""
+        popular_books_id = CheckedOutBook.select('book_id')[0:11]
+        popular_books = []
+        for book in len(popular_books_id):
+            popular_books.append(
+                Book.select('title', 'author').where(Book.book_id == book).get()
+            )
+
+        return popular_books
+
+    @classmethod
+    def get_member_checked_out_books(cls, email):
+        members_id = CheckedOutBook.select('member_id')
+        for id in members_id:
+            member = Member.get(Member.member_id == id)
+            if member.email == email:
+                books = CheckedOutBook.select('book_id').where(CheckedOutBook.member_id == id).get()
+                return books
 
     def check_out(self):
         pass
 
     def return_book(self):
-        pass
-
-    def list_books(self):
-        pass
-
-    def search_by_title(self, title: str):
-        pass
-
-    def search_by_author(self, author: str):
-        pass
-
-    def get_popular_books(self):
-        """This method will give 10 books"""
-        pass
-
-    def get_number_of_books(self, book_id: int):
-        pass
-
-
-class Member:
-
-    def __init__(self,
-                 name: str,
-                 email: str,
-                 address: str,
-                 books_checked_out: list = None):
-        self.name = name
-        self.email = email
-        self.address = address
-        self.books_checked_out = books_checked_out
-
-    def __str__(self):
-        return f'Member: {self.name}'
-
-    def __repr__(self):
-        return f'Member(name="{self.name}", ' \
-               f'email="{self.email}", ' \
-               f'address="{self.address}",' \
-               f'books_checked_out="{self.books_checked_out}")'
-
-    def get_books_checked_out(self):
         pass
 
 
