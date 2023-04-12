06dfe9612d6eb28ca6cfcf32f6951fcbc33527be
iamnovichek <rungotika@gmail.com>
2023-04-12 18:50:50 +0200

    code refactoring

--- lib_catalog.py
+++ lib_catalog.py
@@ -1,15 +1,13 @@
 from peewee import *
+import uuid
 from abc import ABC
 import json
-
-# date manager
 
 # database - SQLite
 DB = SqliteDatabase('library.db')
 
 
-# DB.connect()
-# DB.create_tables(['members', 'books', 'checked_out_books'])
+# date manager
 
 class DataManager(ABC):
     def add_book(self, *args, **kwargs):
@@ -23,15 +21,18 @@ class DataManager(ABC):
 
 
 class Member(Model):
-    member_id = AutoField(primary_key=True,
-                          unique=True)
+    member_id = UUIDField(primary_key=True,
+                          unique=True,
+                          default=uuid.uuid4)
     name = CharField(max_length=60)
     address = CharField(max_length=255)
     email = CharField(max_length=255,
                       unique=True)
 
     def __str__(self):
-        return f'{self.name}'
+        return f'Name: {self.name}, \n' \
+               f'Email: {self.email}, \n' \
+               f'Address: {self.address} \n'
 
     class Meta:
         database = DB
@@ -39,17 +40,17 @@ class Member(Model):
 
 
 class Book(Model):
-    book_id = AutoField(primary_key=True,
-                        unique=True)
     title = CharField(max_length=30)
     author = CharField(max_length=30)
     publisher = CharField(max_length=100)
-    ISBN = CharField(max_length=13)
-    # checked_out = BooleanField() --> to do
-    borrowers = TextField()  # will be a json array
+    ISBN = CharField(max_length=13,
+                     primary_key=True,
+                     unique=True)
+    checked_out = BooleanField(default=False)
 
     def __str__(self):
-        return f'Title: {self.title}/ Author: {self.author}'
+        return f'Title: {self.title}\n' \
+               f'Author: {self.author}\n'
 
     class Meta:
         database = DB
@@ -57,108 +58,13 @@ class Book(Model):
 
 
 class CheckedOutBook(Model):
-    user_id = ForeignKeyField(Member, backref='member_id')
+    member_id = ForeignKeyField(Member,
+                                primary_key=True)
     book_id = ForeignKeyField(Book)
 
     class Meta:
         database = DB
         table_name = 'checked_out_books'
-
-
-class Library(DataManager):
-    # must have checkout system
-
-    @classmethod
-    def add_book(cls,
-                 title: str,
-                 author: str,
-                 publisher: str,
-                 ISBN: int):
-        assert len(str(ISBN)) == 13, 'Enter 13 numbers'
-        book = Book.create(
-            title=title,
-            author=author,
-            publisher=publisher,
-            ISBN=ISBN,
-            borrowers=None
-            # my_list = ['item 1', 'item 2', 'item 3']
-            # json_data = json.dumps(my_list)
-        )
-        book.save()
-
-        return book
-
-    @classmethod
-    def add_member(cls,
-                   name: str,
-                   email: str,
-                   address: str):
-        member = Member.create(
-            name=name,
-            email=email,
-            address=address
-        )
-        member.save()
-
-        return member
-
-    @classmethod
-    def delete_member(cls, email: str):
-        member = Member.select().where(Member.email == email).get()
-        member.delete_instance()
-
-        return member
-
-    @classmethod
-    def list_members(cls):
-        all_members = Member.select()
-
-        return all_members
-
-    @classmethod
-    def list_books(cls):
-        all_books = Book.select('title', 'author')
-
-        return all_books
-
-    @classmethod
-    def search_book_by_title(cls, title: str):
-        book = Book.select('title').where(Book.title == title).get()
-
-        return book
-
-    @classmethod
-    def search_books_by_author(cls, author: str):
-        books = Book.select('author').where(Book.author == author).get()
-
-        return books
-
-    @classmethod
-    def get_popular_books(cls):
-        """This method will give 10 latest books checked out"""
-        popular_books_id = CheckedOutBook.select('book_id')[0:11]
-        popular_books = []
-        for book in len(popular_books_id):
-            popular_books.append(
-                Book.select('title', 'author').where(Book.book_id == book).get()
-            )
-
-        return popular_books
-
-    @classmethod
-    def get_member_checked_out_books(cls, email):
-        members_id = CheckedOutBook.select('member_id')
-        for id in members_id:
-            member = Member.get(Member.member_id == id)
-            if member.email == email:
-                books = CheckedOutBook.select('book_id').where(CheckedOutBook.member_id == id).get()
-                return books
-
-    def check_out(self):
-        pass
-
-    def return_book(self):
-        pass
 
 
 class CheckOut:
@@ -189,3 +95,107 @@ class CheckOut:
         member = Member.select().where(Member.name == name and Member.email == email).get()
 
         return member
+
+
+class Library(DataManager, CheckOut):
+    # must have checkout system
+
+    @staticmethod
+    def add_book(title: str,  # good
+                 author: str,
+                 publisher: str,
+                 ISBN: int):
+        assert len(str(ISBN)) == 13, 'Enter 13 numbers'
+        new_book = Book.create(
+            title=title,
+            author=author,
+            publisher=publisher,
+            ISBN=ISBN,
+            borrowers=0
+        )
+        new_book.save()
+
+        return new_book
+
+    @staticmethod
+    def add_member(name: str,  # good
+                   email: str,
+                   address: str):
+        member = Member.create(
+            name=name,
+            email=email,
+            address=address
+        )
+        member.save()
+
+        return member
+
+    @staticmethod
+    def delete_member(email: str):  # good
+        member = Member.get(Member.email == email)
+        member.delete_instance()
+
+    @staticmethod
+    def list_members():  # good
+        all_members = Member.select()
+
+        return all_members
+
+    @staticmethod
+    def list_books():  # good
+        all_books = Book.select()
+
+        return all_books
+
+    @staticmethod
+    def search_book_by_title(title: str):  # good
+        book = Book.get(Book.title == title)
+
+        return book
+
+    @staticmethod
+    def search_books_by_author(author: str):  # good
+        books = Book.select().where(Book.author == author)
+
+        return books
+
+    @classmethod
+    def get_popular_books(cls):
+        """This method will give 10 latest books checked out"""
+        popular_books = CheckedOutBook.select()
+        try:
+            popular_books = [popular_books[i] for i in range(10)]
+        except StopIteration:
+            popular_books = [popular_books[i] for i in len(popular_books)]
+
+        return [cls._search_book_by_isbn(book.ISBN) for book in popular_books]
+
+    @staticmethod
+    def _search_book_by_isbn(ISBN: int):
+        return Book.get(Book.ISBN == ISBN)
+
+    @classmethod
+    def get_member_checked_out_books(cls,
+                                     email: str):
+        member = Member.get(Member.email == email)
+        books_id = CheckedOutBook.select('book_id').where(CheckedOutBook.member_id == member.member_id)
+
+        return [cls._search_book_by_isbn(i) for i in books_id]
+
+    @classmethod
+    def check_out_specific_book(cls):
+        pass
+
+    @classmethod
+    def check_out_popular_book(cls):
+        pass
+
+    @classmethod
+    def check_out_book_by_author(cls):
+        pass
+
+    @classmethod
+    def return_book(cls,
+                    title: str,
+                    author: str):
+        pass
